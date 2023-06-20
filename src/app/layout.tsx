08009764a2ce382c9ba2c7dc3bfd54529ebbc2a7
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-06-20 21:31:46 +0330

    ✨ feat:Implement Footer component.(#2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Footer } from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
