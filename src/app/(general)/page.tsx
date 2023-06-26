2be0f616e15a0cc147e7f92e9265809603ce0e78
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-06-26 23:53:54 +0330

    ✨ feat:Add hero component in Home.

--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/hero/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+    <main className="flex min-h-screen flex-col items-center justify-between bg-white mx-auto max-w-full">
+      <Hero />
     </main>
   );
 }
