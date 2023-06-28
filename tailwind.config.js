f49a6c49b87bcdccf1112a63c84621b8cd5d9c5b
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-06-28 17:02:32 +0330

    ✨ feat:Add new 'xs' breakpoint to screens.
    Introducing the 'xs' breakpoint for screens, catering to extra small devices and improving responsiveness.
    
    Fixes #6

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "500px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      minWidth: {
+        "1140px": "71.25rem",
       },
       maxWidth: {
         90: "90rem",
