d7be875444bb8977ef851d9712d8aef20622c232
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-07-20 23:30:49 +0330

    ✨ feat:add xs breakpoing to config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
+    screens: {
+      xs: { max: "639px" },
+      ...defaultTheme.screens,
+    },
     extend: {
-      screens: {
-        xs: "500px",
-      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
