ee8171f27e597afa6de84020f605e9efa818f090
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-07-21 22:55:56 +0330

    🐛 fix(ui):fix responsive issues.

--- src/components/presentational/Hero.tsx
+++ src/components/presentational/Hero.tsx
@@ -5,20 +5,20 @@ import { ArrowRight } from "react-huge-icons/outline";
 
 const Hero = function () {
   return (
-    <div className="relative bg-dark5  xl:max-w-[1380px]  lg:w-full">
-      <div className=" lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
-        <div className="ml-0 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
-          <div className="relative mx-auto   max-w-2xl lg:mx-0">
-            <p className="mt-6 text-dark font-400 font-jostBodyRegular break-words text-3xl">
+    <div className="relative bg-dark5  max-w-[1380px]  lg:w-full md:w-full">
+      <div className=" grid lg:grid-cols-12  lg:gap-x-8 lg:px-8 md:grid-cols-2  md:px-10">
+        <div className="ml-0 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6 lg:mx-auto w-fit xs:mx-auto">
+          <div className="relative  max-w-2xl lg:mx-0">
+            <p className="mt-6 text-dark xs:text-center lg:text-left font-400 font-jostBodyRegular break-words text-3xl">
               Classic Exclusive
             </p>
-            <h3 className="mt-6 text-dark font-700 font-jostBodyBold break-words text-4xl">
+            <h3 className="mt-6 text-dark font-700 xs:text-center sm:text-center lg:text-left font-jostBodyBold break-words text-4xl">
               Women’s Collection
             </h3>
-            <p className="mt-6 text-dark font-400 font-jostBodyRegular break-words text-3xl">
+            <p className="mt-6 text-dark font-400 xs:text-center sm:text-center lg:text-left font-jostBodyRegular break-words text-3xl">
               UPTO 40% OFF
             </p>
-            <div className="mt-10 flex items-center gap-x-6">
+            <div className="mt-10 flex items-center xs:justify-center md:justify-normal sm:justify-center gap-x-6">
               <a
                 href="#"
                 className="rounded-md  bg-dark text-16 font-jostBodyRegular px-5 py-5 text-white"
@@ -32,15 +32,15 @@ const Hero = function () {
               </a>
             </div>
             <div className="text-white mb-24 ml-10 font-700 opacity-60 font-jostBodyBold  text-241  relative break-words">
-              <p className="absolute">BESTSELLE</p>
+              <p className="absolute">BESTSELLER</p>
             </div>
           </div>
         </div>
-        <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:ml-0">
+        <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:ml-0 ">
           <Image
             width={593}
             height={884}
-            className="aspect-[3/2] w-auto bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full sm:aspect-auto"
+            className="aspect-[3/2] w-auto bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full sm:aspect-auto md:bottom-0 md:absolute"
             src="/images/photo.jpg"
             alt=""
           />
