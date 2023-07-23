b432f11328bca1da3a2bd6b06305a85019fd887b
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-07-23 17:24:16 +0330

    fix: fix hero responsive issue in md breakpoint.

--- src/components/presentational/Hero.tsx
+++ src/components/presentational/Hero.tsx
@@ -5,17 +5,17 @@ import { ArrowRight } from "react-huge-icons/outline";
 
 const Hero = function () {
   return (
-    <div className="relative bg-dark5  max-w-[1380px]  lg:w-full md:w-full">
+    <div className="relative bg-dark5  max-w-[1380px]  lg:w-full md:w-full sm:w-full">
       <div className=" grid lg:grid-cols-12  lg:gap-x-8 lg:px-8 md:grid-cols-2  md:px-10">
-        <div className="ml-0 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6 lg:mx-auto w-fit xs:mx-auto">
+        <div className="ml-0 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6 sm:mx-auto lg:mx-auto w-fit xs:mx-auto ">
           <div className="relative  max-w-2xl lg:mx-0">
-            <p className="mt-6 text-dark xs:text-center lg:text-left font-400 font-jostBodyRegular break-words text-3xl">
+            <p className="mt-6 text-dark xs:text-center sm:text-center md:text-left lg:text-left font-400 font-jostBodyRegular break-words text-3xl">
               Classic Exclusive
             </p>
-            <h3 className="mt-6 text-dark font-700 xs:text-center sm:text-center lg:text-left font-jostBodyBold break-words text-4xl">
+            <h3 className="mt-6 text-dark font-700 xs:text-center md:text-left sm:text-center lg:text-left font-jostBodyBold break-words text-4xl">
               Women’s Collection
             </h3>
-            <p className="mt-6 text-dark font-400 xs:text-center sm:text-center lg:text-left font-jostBodyRegular break-words text-3xl">
+            <p className="mt-6 text-dark font-400 xs:text-center md:text-left sm:text-center lg:text-left font-jostBodyRegular break-words text-3xl">
               UPTO 40% OFF
             </p>
             <div className="mt-10 flex items-center xs:justify-center md:justify-normal sm:justify-center gap-x-6">
@@ -40,7 +40,7 @@ const Hero = function () {
           <Image
             width={593}
             height={884}
-            className="aspect-[3/2] w-auto bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full sm:aspect-auto md:bottom-0 md:absolute"
+            className="aspect-[3/2] w-auto bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full sm:aspect-auto md:bottom-0"
             src="/images/photo.jpg"
             alt=""
           />
