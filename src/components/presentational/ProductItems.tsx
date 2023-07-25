9c89159b30f2a201dbda953caaa4d29aebbe458c
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-07-25 20:45:10 +0330

    ✨ feat(ui): Implement SortProducts component.

--- src/components/presentational/ProductItems.tsx
+++ src/components/presentational/ProductItems.tsx
@@ -1,8 +1,13 @@
-import { ArrowsRightLeftIcon, HeartIcon } from "@heroicons/react/20/solid";
+import {
+  ArrowsRightLeftIcon,
+  ChevronDownIcon,
+  HeartIcon,
+} from "@heroicons/react/20/solid";
 import Image from "next/image";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { Eye } from "react-huge-icons/outline";
 import ProductCategories from "./ProductCategories";
+import { Menu, Transition } from "@headlessui/react";
 
 const products = [
   {
@@ -97,6 +102,16 @@ const products = [
   },
   // More products...
 ];
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+const sortOptions = [
+  { name: "Most Popular", href: "#", current: true },
+  { name: "Best Rating", href: "#", current: false },
+  { name: "Newest", href: "#", current: false },
+  { name: "Price: Low to High", href: "#", current: false },
+  { name: "Price: High to Low", href: "#", current: false },
+];
 type ShowStateType = {
   [key: string]: boolean;
 };
@@ -107,6 +122,53 @@ const ProductItems = function () {
   };
   return (
     <div className="grid grid-cols-3 gap-x-10">
+      <Menu as="div" className="relative flex col-span-3 justify-between ">
+        <h4 className=" font-jostBodyRegular text-dark text-[16px] tracking-tight">
+          Showing 1–16 of 72 results
+        </h4>
+        <div>
+          <Menu.Button className="group text-dark text-16 inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+            Sort
+            <ChevronDownIcon
+              className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              {sortOptions.map((option) => (
+                <Menu.Item key={option.name}>
+                  {({ active }) => (
+                    <a
+                      href={option.href}
+                      className={classNames(
+                        option.current
+                          ? "font-medium text-gray-900"
+                          : "text-gray-500",
+                        active ? "bg-gray-100" : "",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      {option.name}
+                    </a>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
       {products.map((product) => (
         <div
           onMouseEnter={() => toggleHandler(product.id, true)}
@@ -151,13 +213,13 @@ const ProductItems = function () {
               Add to Cart
             </button>
           </div>
-          <h3 className="mt-4 font-700 text-16">
+          <h3 className="mt-4 font-700 text-dark text-16">
             <a href={product.href}>
               <span className="absolute inset-0" />
               {product.name}
             </a>
           </h3>
-          <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+          <p className="mt-1 text-sm text-dark">{product.color}</p>
           <div className="flex justify-start gap-2 items-center">
             <p className="mt-1 text-16 font-700 text-dark">{product.price}</p>
             <p className="mt-[2px] text-gray text-16 font-400 font-jostBodyRegular line-through">
---
+++ src/components/presentational/SortProducts.tsx
@@ -0,0 +1,16 @@
+import { Fragment, useState } from "react";
+
+const sortOptions = [
+  { name: "Most Popular", href: "#", current: true },
+  { name: "Best Rating", href: "#", current: false },
+  { name: "Newest", href: "#", current: false },
+  { name: "Price: Low to High", href: "#", current: false },
+  { name: "Price: High to Low", href: "#", current: false },
+];
+const SortProducts = function () {
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+
+  return <></>;
+};
+
+export default SortProducts;
