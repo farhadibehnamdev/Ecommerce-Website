c373638099fa172b07f63a8a86e79c046613ef2d
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-08-13 23:32:39 +0330

    feat(ui): add additionalInfoTab component

---
+++ src/components/presentational/AdditionalInfoTab.tsx
@@ -0,0 +1,17 @@
+const AdditionalInfoTab = function () {
+  return (
+    <div className="pt-5">
+      <p className="text-dark text-16 mb-4">
+        <span className="font-700">Color</span>
+        <span className="font-400 ml-5">
+          Red, Blue, Orange, Black, Green, Yellow
+        </span>
+      </p>
+      <p className="text-dark text-16">
+        <span className="font-700">Size</span>
+        <span className="font-400 ml-7">S, M, L, XL, XXL</span>
+      </p>
+    </div>
+  );
+};
+export default AdditionalInfoTab;
