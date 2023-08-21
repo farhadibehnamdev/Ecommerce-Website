c5a565eb8cb6ea900593834d999dc0b6950a3115
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-08-21 18:54:47 +0330

    feat: add forgotPasswordContainer

---
+++ src/app/(auth)/auth/forgot-password/page.tsx
@@ -0,0 +1,6 @@
+import ForgotPasswordContainer from "@/components/website/containers/ForgotPasswordContainer";
+
+const ForgotPasswordPage = function () {
+  return <ForgotPasswordContainer />;
+};
+export default ForgotPasswordPage;
