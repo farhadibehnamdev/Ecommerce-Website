553605a9d5127788480fd3c84bb02e774e04d259
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-08-26 08:04:39 +0330

    feat: add inter font config

--- src/utils/fontConfig.ts
+++ src/utils/fontConfig.ts
@@ -7,3 +7,17 @@ export const jostBodyBold = localFont({
   src: [{ path: "/fonts/Jost-Bold.ttf", weight: "700", style: "normal" }],
   variable: "--font-jostBold",
 });
+
+export const interRegular = localFont({
+  src: [
+    { path: "/fonts/inter/Inter-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: "--font-interRegular",
+});
+
+export const interSemibold = localFont({
+  src: [
+    { path: "/fonts/inter/Inter-SemiBold.ttf", weight: "600", style: "normal" },
+  ],
+  variable: "--font-interSemiBold",
+});
