349de01d2f09e6ae8e3b1d251dd245b41e5e7c47
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-09-20 22:50:13 +0330

    feat: change color of sidebar from gray to zinc

--- src/components/dashboard/presentational/Sidebar.tsx
+++ src/components/dashboard/presentational/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import "@/styles/tailwind.dashboard.css";
+import "@/styles/globals.css";
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 
@@ -194,7 +194,7 @@ export default function Sidebar() {
       {/* Static sidebar for desktop */}
       <div className="hidden  text-sm  lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex grow  flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
+        <div className="flex grow  flex-col gap-y-5 overflow-y-auto border-r border-light text-zinc-800 bg-white px-6 pb-4">
           <div className="flex h-16 shrink-0 items-center">
             <Image
               width={0}
