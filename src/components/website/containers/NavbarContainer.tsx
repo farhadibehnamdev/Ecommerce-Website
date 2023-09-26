d5ea6214a51da805933846f0210d03b5e1ac3e6e
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-09-26 23:27:40 +0330

    feat: add useContext to share state with Navbar

--- src/components/website/containers/NavbarContainer.tsx
+++ src/components/website/containers/NavbarContainer.tsx
@@ -1,28 +1,22 @@
 "use client";
 import api from "@/api/api";
 import Navbar from "@/components/website/presentational/Navbar";
-import { useAppSelector } from "@/store/hooks";
-import { loginSelector, logout } from "@/store/slices/auth/loginSlice";
-import { useDispatch } from "react-redux";
+import SessionContext, { ISessionContext } from "@/contexts/SessionContext";
+import { useQueryClient } from "@tanstack/react-query";
+import { useContext, useReducer, useState } from "react";
 const NavbarContainer = function () {
-  const dispatch = useDispatch();
-  const { isAuth, data } = useAppSelector(loginSelector);
-
-  if (data?.tokens?.access?.token && data?.tokens?.access?.expires) {
-    const { token, expires } = data.tokens.access;
-    const date = new Date(expires);
-    const now = new Date();
-    if (now.getTime() > date.getTime()) {
-      let status = false;
-      api.get("auth/logout").then((res) => {
-        if (res.status === 200) {
-          status = true;
-        }
-      });
-      if (status) dispatch(logout());
-    }
-  }
-  return <Navbar isAuth={isAuth} />;
+  const query = useQueryClient();
+  const { user, isLoading, refetch, isFetching } =
+    useContext<ISessionContext>(SessionContext);
+  return (
+    <Navbar
+      user={user!}
+      isLoading={isLoading}
+      isFetching={isFetching}
+      refetch={refetch}
+      query={query}
+    />
+  );
 };
 
 export default NavbarContainer;
