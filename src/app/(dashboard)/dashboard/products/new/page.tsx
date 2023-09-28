f964ec795544a5b5556488d4ae9e510435edb1d0
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-09-28 21:59:36 +0330

    feat: add addProductPage and productsPage

---
+++ src/app/(dashboard)/dashboard/products/new/page.tsx
@@ -0,0 +1,7 @@
+import AddProductContainer from "@/components/dashboard/containers/AddProductContainer";
+
+const AddProductPage = function () {
+  return <AddProductContainer />;
+};
+
+export default AddProductPage;
---
+++ src/app/(dashboard)/dashboard/products/page.tsx
@@ -0,0 +1,8 @@
+import ProductsContainer from "@/components/dashboard/containers/ProductsContainer";
+import TableUI from "@/components/dashboard/presentational/Table";
+
+const ProductsPage = function () {
+  return <ProductsContainer />;
+};
+
+export default ProductsPage;
