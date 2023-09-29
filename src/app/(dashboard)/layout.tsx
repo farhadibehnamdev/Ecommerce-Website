d9ef4ff9329b33c7a1c0224780095e195efd829d
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-09-29 23:22:20 +0330

    feat: add sessionProvider and NavbarContainer

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,17 +1,28 @@
 import "@/styles/globals.css";
 import Sidebar from "@/components/dashboard/presentational/Sidebar";
 import Navbar from "@/components/dashboard/presentational/Navbar";
-const DashboardLayout = function ({ children }: { children: React.ReactNode }) {
+import TanstakProvider from "@/providers/TanstackProvider";
+import SessionProvider from "@/providers/SessionProvider";
+import NavbarContainer from "@/components/dashboard/containers/NavbarContainer";
+
+interface IDashboardLayout {
+  children: React.ReactNode;
+}
+const DashboardLayout = function ({ children }: IDashboardLayout) {
   return (
     <html className="h-full bg-white">
       <body className="h-full font-interRegular">
-        <Sidebar />
-        <Navbar />
-        <div className="lg:pl-72 pt-16">
-          <main className="py-10">
-            <div className="px-4 sm:px-6 lg:px-8">{children}</div>
-          </main>
-        </div>
+        <TanstakProvider>
+          <Sidebar />
+          <SessionProvider>
+            <NavbarContainer />
+            <div className="lg:pl-72 pt-16">
+              <main className="py-10">
+                <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+              </main>
+            </div>
+          </SessionProvider>
+        </TanstakProvider>
       </body>
     </html>
   );
