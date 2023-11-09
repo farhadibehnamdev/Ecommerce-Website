8c975b21a14ac1b1005f993a569a198daa391b86
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-11-09 22:04:35 +0330

    feat: add users' role to navbar

--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -26,8 +26,10 @@ const userNavigation = [
 
 const Navbar = function () {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
-  const { user, isLoading, isFetching, refetch } =
-    useContext<ISessionContext>(SessionContext);
+  // const { user, isLoading, isFetching, refetch, status } =
+  //   useContext<ISessionContext>(SessionContext);
+  const { user, isLoading } = useContext<ISessionContext>(SessionContext);
+
   return (
     <div className="lg:pl-72 text-zinc-900 border-b border-light">
       <div className="sticky top-0 z-40 font-jostBodyBold flex h-16 shrink-0 items-center gap-x-4 bg-white px-4  sm:gap-x-6 sm:px-6 lg:px-8">
@@ -44,26 +46,24 @@ const Navbar = function () {
             <PiSidebarDuotone className="w-5 h-5 text-dark cursor-pointer rounded-lg justify-center items-center gap-1 inline-flex" />
           </button>
           {isLoading ? (
-            <>
-              <div className="max-w-[200px] w-full flex items-center gap-3">
-                <div className="flex gap-2">
-                  <Skeleton className="flex rounded-full w-10 h-10" />
-                  <Skeleton className="flex rounded-full w-10 h-10" />
-                </div>
-                <div className="w-full flex flex-col gap-2">
-                  <Skeleton className="h-6 w-4/5 rounded-lg" />
-                </div>
+            <div className="max-w-[200px] w-full flex items-center gap-3">
+              <div className="flex gap-2">
+                <Skeleton className="flex rounded-full w-10 h-10" />
+                <Skeleton className="flex rounded-full w-10 h-10" />
               </div>
-            </>
+              <div className="w-full flex flex-col gap-2">
+                <Skeleton className="h-6 w-4/5 rounded-lg" />
+              </div>
+            </div>
           ) : (
             <div className="flex items-center gap-x-4 lg:gap-x-6">
               {/* <button
-                type="button"
-                className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">View notifications</span>
-                <BellIcon className="h-6 w-6" aria-hidden="true" />
-              </button> */}
+          type="button"
+          className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+        >
+          <span className="sr-only">View notifications</span>
+          <BellIcon className="h-6 w-6" aria-hidden="true" />
+        </button> */}
               <Badge content="99+" shape="circle" color="danger">
                 <Button
                   radius="full"
@@ -91,8 +91,8 @@ const Navbar = function () {
                       src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
                     }}
                     className="transition-transform"
-                    description="@tonyreichert"
-                    name="Tony Reichert"
+                    description={`${user?.role}`}
+                    name={`${user?.firstName} ${user?.lastName}`}
                   />
                 </DropdownTrigger>
                 <DropdownMenu aria-label="User Actions" variant="flat">
