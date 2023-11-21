bd76b49074ac82c8a59b37d18f51a56596293854
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-11-21 23:42:07 +0330

    refactor: remove getting cookie because it was unnecessary

--- src/api/interceptors/index.ts
+++ src/api/interceptors/index.ts
@@ -3,29 +3,22 @@ import { axiosInstance } from "../api";
 import Cookies from "universal-cookie";
 const cookie = new Cookies();
 export const refreshToken = async () => {
-  if (cookie.get("refToken")) {
-    try {
-      const response = await axiosInstance.post("auth/generate/tokens");
+  try {
+    const response = await axiosInstance.post("auth/generate/tokens");
 
-      const newAccessToken = response.data?.tokens?.access.token;
-      axiosInstance.defaults.headers.common[
-        "Authorization"
-      ] = `${newAccessToken}`;
+    const newAccessToken = response.data?.tokens?.access.token;
+    axiosInstance.defaults.headers.common[
+      "Authorization"
+    ] = `${newAccessToken}`;
 
-      return newAccessToken;
-    } catch (error) {
-      return Promise.reject(error);
-    }
+    return newAccessToken;
+  } catch (error) {
+    return Promise.reject(error);
   }
 };
 
 export const requestInterceptor = (config: InternalAxiosRequestConfig) => {
-  const token = cookie.get("accToken");
-  if (token) {
-    config.headers!.Authorization = `${token}`;
-  }
   return config;
-  // }
 };
 
 export const requestInterceptorError = (err: any) => {
@@ -40,7 +33,12 @@ export const responseInterceptorError = async (err: any) => {
   const originalRequest = err.config;
   if (err.response.status === 401 && !originalRequest._retry) {
     originalRequest._retry = true;
-    await refreshToken();
+    try {
+      await refreshToken();
+    } catch (error: any) {
+      return Promise.reject(error);
+    }
+    return axiosInstance(originalRequest);
   }
   return Promise.reject(err);
 };
