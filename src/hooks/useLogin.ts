02b1482e3b077aae0c81f9c900925a53449fcfbd
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-11-27 23:09:32 +0330

    refactor: revise user's login to custom hook

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,40 +1,57 @@
-import { logingUserApi } from "@/api/userApi";
 import { LoginFormType } from "@/components/website/Login";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import Cookies from "universal-cookie";
-const cookie = new Cookies();
+import { useRef, useState } from "react";
+import { loginUserApi } from "@/api/userApi";
+const cookies = new Cookies();
 
 const userLogin = async function (data: LoginFormType) {
-  const response = await logingUserApi(data);
+  const response = await loginUserApi(data);
+  console.log("response.data :: ", response.data);
+  if (response.status >= 400) throw new Error(response.statusText);
   if (response.status === 201) {
-    cookie.set("access_token", response.data.tokens?.access, {
+    console.log("response.data :: ", response.data);
+    alert("stop");
+    cookies.set("access_token", response.data.tokens?.access.token, {
       path: "/",
       expires: new Date(response.data.tokens?.access.expires),
+      httpOnly: true,
     });
-    cookie.set("refresh_token", response.data.tokens?.refresh, {
+    cookies.set("refresh_token", response.data.tokens?.refresh.token, {
       path: "/",
       expires: new Date(response.data.tokens?.refresh.expires),
+      httpOnly: true,
     });
-    return response.data;
+    return response.data.user;
   } else {
-    throw new Error(response.data.message);
+    throw new Error(response.error.message);
   }
 };
 
 const useLogin = function () {
+  const [isLoadingState, setIsLoadingState] = useState<boolean>(false);
+  const toastId = useRef<string | null>(null);
   const router = useRouter();
-  const toastId = toast.loading("Loading...");
   const loginMutation = useMutation({
+    mutationKey: ["userlogin"],
     mutationFn: userLogin,
+    onMutate() {
+      if (!isLoadingState) {
+        toastId.current = toast.loading("Loading...");
+        setIsLoadingState(true);
+      }
+    },
     onError(error, variables, context) {
-      toast.error(error.message, { id: toastId });
+      toast.error(error.message, { id: toastId.current! });
+      setIsLoadingState(false);
     },
     onSuccess(data, variables, context) {
       toast.success("You have successfully logged in", {
-        id: toastId,
+        id: toastId.current!,
       });
+      setIsLoadingState(false);
       router.push("/");
     },
   });
