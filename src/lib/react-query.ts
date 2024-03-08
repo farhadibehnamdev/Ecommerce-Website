1b7fd199dac882680767b3ae2ab2dd1fea32cb8d
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2024-03-09 01:04:03 +0330

    refactor: separate react-query configuration from provider

---
+++ src/lib/react-query.ts
@@ -0,0 +1,18 @@
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError(error, query) {
+      //show notification
+    },
+  }),
+
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      throwOnError: false,
+      gcTime: 1000 * 60 * 60 * 24,
+    },
+  },
+});
