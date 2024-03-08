869b16bd5562e6f342ac970a726ec838b7b8f368
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2024-03-09 01:30:00 +0330

    chore: update package.json and vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
@@ -6,11 +6,16 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: ["tests/vitest.setup.ts"],
+    environment: "happy-dom",
+    setupFiles: ["vitest-setup/vitest.setup.ts"],
     coverage: {
       provider: "v8",
     },
+    exclude: [
+      ...configDefaults.exclude,
+      "**/tests/integration/**",
+      "**/tests/e2e/**",
+    ],
   },
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
