6338a9bdeb156890c2a36a0a73c2cec3eb7b0435
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2024-03-16 17:32:52 +0330

    refactor: remove unnecessary files

--- src/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-import { IStore } from "./type";
-
-export const useStore = create<IStore>((set) => ({
-  productId: null,
-  setProductId: (id) => set({ productId: id }),
-}));
