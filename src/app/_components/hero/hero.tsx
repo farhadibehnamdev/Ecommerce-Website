924195c6d29167aa4ad365831c7fd166227d68a0
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2024-03-21 22:37:18 +0330

    feat: add another h1 element

--- src/app/_components/hero/hero.tsx
+++ src/app/_components/hero/hero.tsx
@@ -8,10 +8,11 @@ export const Hero = function () {
     <section className=" bg-dark5  xl:bg-left mt-5 xl:mt-2">
       <div className="container flex flex-col-reverse items-center justify-center gap-16 xl:flex-row">
         <div className="flex flex-col gap-5 mt-12 pb-5 text-center xl:text-center">
-          <h3 className="text-xl xl:text-2xl text-left">Women’s Collection</h3>
-          <h1 className=" text-3xl lg:text-5xl xl:text-5xl font-black">
-            UPTO 40% OFF
+          <h2 className="text-xl xl:text-2xl text-left">Classic Exclusive</h2>
+          <h1 className="text-3xl lg:text-5xl xl:text-5xl font-black">
+            Women’s Collection
           </h1>
+          <h3 className="text-xl xl:text-2xl text-left">UPTO 40% OFF</h3>
           <Button className="mt-5 max-w-fit bg-dark text-white ">
             <p>Shop Now</p>
             <p className="flex items-end justify-start">
@@ -19,8 +20,10 @@ export const Hero = function () {
             </p>
           </Button>
         </div>
-
         <Image src="/images/photo.jpg" alt="" width={500} height={885} />
+        {/* <div className="text-red-600 font-bold font-700 opacity-60 text-9xl break-words">
+            <p className="absolute">BESTSELLER</p>
+          </div> */}
       </div>
     </section>
   );
